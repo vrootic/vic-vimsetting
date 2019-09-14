8b40fdd2e9a1d2f1c68d6fa84fb587cee0fca9bb
Vic Yang <vrootic@gmail.com>
2019-09-14 15:28:39 +0900

    remove soft link

--- init.sh
+++ init.sh
@@ -3,6 +3,3 @@
 # First clone Vundle from GitHub
 mkdir ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# Create .vimrc soft link to home folder
-ln -s .vimrc ~/.vimrc
