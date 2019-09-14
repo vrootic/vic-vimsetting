aa0f1c9876c302928aeeaf1fcee43f30f886ecac
Vic Yang <vrootic@gmail.com>
2019-09-14 15:31:57 +0900

    fix softlink

--- init.sh
+++ init.sh
@@ -3,3 +3,6 @@
 # First clone Vundle from GitHub
 mkdir ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Create soft link to home directory
+ln -s ~/vic-vimsetting/.vimrc ~/.vimrc
